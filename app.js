ff1361586f169151311adef768341640ab0032c1
2Kelvin <kvin.mwangi254@gmail.com>
2022-11-06 23:46:15 +0300

    used component's props inside the event handler

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-function AButton() {
-    // event handler button click
-    function handleClick() {
-        alert("You clicked the button!");
-    }
+function Button(_ref) {
+    var children = _ref.children,
+        message = _ref.message;
 
     return React.createElement(
         "button",
         {
             className: "button-30",
-            onClick: handleClick
+            onClick: function onClick() {
+                return alert(message);
+            }
         },
-        "Button"
+        children
     );
 }
 
@@ -18,7 +18,16 @@ export default function App() {
     return React.createElement(
         "div",
         { className: "app" },
-        React.createElement(AButton, null)
+        React.createElement(
+            Button,
+            { message: "Playing your song" },
+            "Play Song"
+        ),
+        React.createElement(
+            Button,
+            { message: "Uploading your image" },
+            "Upload Image"
+        )
     );
 }
 
@@ -28,6 +37,20 @@ var root = ReactDOM.createRoot(rootNode);
 root.render(React.createElement(App, null));
 
 // NOTES 
-// event handlers are usually defined inside your component(s)
+// event handlers are usually defined INSIDE your component(s)
 // naming: start with the name 'handle' followed by the name of the event e.g. click, hover, focus
-//
+// event handlers passed in as props MUST be 'passed' not 'called'
+
+// you could also add inline event listeners
+// ...it's just adding the button click logic directly in the onClick function (without having to define a function name & passing it in as a prop)
+// to be more concise, use an arrow function for the inline event handler(s)
+// inline event handlers are convinient for short functions
+
+// functions passed to event handlers as props should be passed NOT called
+// passing the function tells react to wait till an event occurs e.g. button is clicked for it to call the function
+// calling the function calls it when rendering occurs without an event even triggering it wgich is not what you want
+// using the inline format, make sure to add the function/ arrow function syntax to the event handler to prevent it from firing when the component renders
+// basically pass a function to an event handler to prevent it from firing every time the component renders
+// ...and only ensure it fires when an event occurs
+
+// since an event handler is defined inside a component, it has access to all its props
--- src/app.js
+++ src/app.js
@@ -1,15 +1,10 @@
-function AButton() {
-    // event handler button click
-    function handleClick() {
-        alert("You clicked the button!");
-    }
-
+function Button({ children, message }) {
     return (
         <button
             className="button-30"
-            onClick={handleClick}
+            onClick={() => alert(message)}
         >
-            Button
+            {children}
         </button>
     );
 }
@@ -17,7 +12,12 @@ function AButton() {
 export default function App() {
     return (
         <div className="app" >
-            <AButton />
+            <Button message="Playing your song">
+                Play Song
+            </Button>
+            <Button message="Uploading your image">
+                Upload Image
+            </Button>
         </div>
     );
 }
@@ -28,6 +28,20 @@ const root = ReactDOM.createRoot(rootNode);
 root.render(<App />);
 
 // NOTES 
-// event handlers are usually defined inside your component(s)
+// event handlers are usually defined INSIDE your component(s)
 // naming: start with the name 'handle' followed by the name of the event e.g. click, hover, focus
-// 
+// event handlers passed in as props MUST be 'passed' not 'called'
+
+// you could also add inline event listeners
+// ...it's just adding the button click logic directly in the onClick function (without having to define a function name & passing it in as a prop)
+// to be more concise, use an arrow function for the inline event handler(s)
+// inline event handlers are convinient for short functions
+
+// functions passed to event handlers as props should be passed NOT called
+// passing the function tells react to wait till an event occurs e.g. button is clicked for it to call the function
+// calling the function calls it when rendering occurs without an event even triggering it wgich is not what you want
+// using the inline format, make sure to add the function/ arrow function syntax to the event handler to prevent it from firing when the component renders
+// basically pass a function to an event handler to prevent it from firing every time the component renders
+// ...and only ensure it fires when an event occurs
+
+// since an event handler is defined inside a component, it has access to all its props
